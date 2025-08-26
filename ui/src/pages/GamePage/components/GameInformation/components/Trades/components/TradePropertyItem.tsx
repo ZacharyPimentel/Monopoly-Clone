25cc966cc321c2ee01b661a02f7f69fd9537173b
Zac Pimentel <Zachary.Pimentel@gmail.com>
2025-08-26 07:44:17 -0400

    fixing ui in trade modals

--- ui/src/pages/GamePage/components/GameInformation/components/Trades/components/TradePropertyItem.tsx
+++ ui/src/pages/GamePage/components/GameInformation/components/Trades/components/TradePropertyItem.tsx
@@ -1,17 +1,21 @@
 import { useFormContext } from "react-hook-form";
 import { BoardSpace } from "@generated/index";
+import { useMemo } from "react";
 
 export const TradePropertyItem:React.FC<{space:BoardSpace, formControl:string}> = ({space,formControl}) => {
     const form = useFormContext();
 
     const gamePropertyId = space.property?.gamePropertyId
     const selectedPropertyIds = form.watch(formControl)
+
+    const isPropertySelected = useMemo( () => {
+        return selectedPropertyIds.includes(gamePropertyId)
+    },[selectedPropertyIds,gamePropertyId])
     return (
         <button 
-            style={{backgroundColor: selectedPropertyIds.includes(gamePropertyId) ? 'black' : "white"}}
             onClick={() => {
             //remove if already selected
-            if(selectedPropertyIds.includes(gamePropertyId)){
+            if(isPropertySelected){
                 form.setValue(
                     formControl,
                     selectedPropertyIds.filter( (id:number) => id !== gamePropertyId))
@@ -21,8 +25,8 @@ export const TradePropertyItem:React.FC<{space:BoardSpace, formControl:string}> 
                 form.setValue(formControl,[...form.getValues(formControl),gamePropertyId])
             }
             
-        }} className='flex w-full bg-white p-[5px] hover:bg-black hover:text-white'>
-            {space.boardSpaceName}
+        }} className={`${isPropertySelected ? 'bg-black text-white' : "bg-white hover:bg-black hover:text-white"} flex justify-between w-full p-[5px]`}>
+            {space.boardSpaceName} <span className='flex w-[20px] h-[20px] rounded-[20px]' style={{backgroundColor:space?.property?.color}}></span>
         </button>
     )
 }
