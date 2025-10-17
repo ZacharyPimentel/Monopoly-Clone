8d004046269a28e699ebeb30fee96f395c64252a
Zac Pimentel <Zachary.Pimentel@gmail.com>
2025-10-16 21:35:52 -0400

    adjusting db player icon paths

--- api/Database/SeedData/PlayerIcon.sql
+++ api/Database/SeedData/PlayerIcon.sql
@@ -1,9 +1,9 @@
 INSERT INTO PlayerIcon (Id, IconUrl, IconName) VALUES
-(1, '/assets/gamePieces/daisy.png','Daisy'),
-(2, '/assets/gamePieces/wizard_boo.png','Boo'),
-(3, '/assets/gamePieces/yoshi.png','Yoshi'),
-(4, '/assets/gamePieces/bowser.png','Bowser'),
-(5, '/assets/gamePieces/peach.png','Peach'),
-(6, '/assets/gamePieces/spike.png','Spike'),
-(7, '/assets/gamePieces/wario.png','Wario'),
-(8, '/assets/gamePieces/dustball.png','Dustball');
+(1, '/monopoly-app/ui/assets/gamePieces/daisy.png','Daisy'),
+(2, '/monopoly-app/ui/assets/gamePieces/wizard_boo.png','Boo'),
+(3, '/monopoly-app/ui/assets/gamePieces/yoshi.png','Yoshi'),
+(4, '/monopoly-app/ui/assets/gamePieces/bowser.png','Bowser'),
+(5, '/monopoly-app/ui/assets/gamePieces/peach.png','Peach'),
+(6, '/monopoly-app/ui/assets/gamePieces/spike.png','Spike'),
+(7, '/monopoly-app/ui/assets/gamePieces/wario.png','Wario'),
+(8, '/monopoly-app/ui/assets/gamePieces/dustball.png','Dustball');
